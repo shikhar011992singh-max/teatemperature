dc259caffe68cfa2c911901517fe5b56e8a043fa
Prakhar Singh <shikhar011992singh@gmail.com>
2026-01-13 20:19:51 +0530

    Initial Commit

---
+++ teatemp.py
@@ -0,0 +1,7 @@
+temperature=50
+while temperature <= 100:
+      #temperature = temperature + 15;
+      temperature += 15
+      print(f"Current temperature: {temperature}")
+
+print(f"Tea is ready to boil")
